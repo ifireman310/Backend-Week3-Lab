57cb1e06a231f382367de6a80aa2d75d0b51679c
unknown <ifireman310@gmail.com>
2024-08-27 20:14:30 -0700

    Solved problems 1 through 3 in lab

--- src/week04/Week04StringBuilderListSetMapLab.java
+++ src/week04/Week04StringBuilderListSetMapLab.java
@@ -6,24 +6,62 @@
 //
 package week04;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Week04StringBuilderListSetMapLab {
 
 	public static void main(String[] args) {
-
+		String linesep = "----------------------------";
+		
 		// 1. Why would we use a StringBuilder instead of a String?
 		// 		a. Instantiate a new StringBuilder
 		//		b. Append the characters 0 through 9 to it separated by dashes
 		// 				Note:  make sure no dash appears at the end of the StringBuilder
-
+		System.out.println(linesep);
+		System.out.println("Problem 1: Stringbuilder");
+		
+		StringBuilder nums = new StringBuilder();
+		for (int i = 0; i < 10; i++) {
+			nums.append(Integer.toString(i)+"-");
+		} 
+		nums.deleteCharAt(nums.length()-1);
+		
+		System.out.println(nums);
+		System.out.println(linesep);
+		System.out.println("");
 		
 		// 2. List of String:
 		//		a. Create a list of Strings 
 		//		b. Add 5 Strings to it, each with a different length
-
+		System.out.println(linesep);
+		System.out.println("Problem 2: List of Strings");
+		
+		List<String> myStringList = new ArrayList<String>();
+		
+		myStringList.add("Is this real life");
+		myStringList.add("Or is this just fantasy");
+		myStringList.add("Caught in a landslide");
+		myStringList.add("No escape from reality");
+		myStringList.add("Hello World!");
+		
+		for (String phrase : myStringList) {
+			System.out.println("phrase: " + phrase + ", length: " + phrase.length());
+		}
+		
+		System.out.println(linesep);
+		System.out.println("");
 		
 		// 3. Write and test a method that takes a list of strings 
 		//			and returns the shortest string
-
+		System.out.println(linesep);
+		System.out.println("Problem 3: Return smallest string in list");
+		System.out.println("The shortest string from the previous list is:");
+		System.out.println(shortestStringInList(myStringList));
+		System.out.println(linesep);
+		System.out.println("");
 		
 		// 4. Write and test a method that takes a list of strings 
 		//			and returns the list with the first and last element switched
@@ -134,7 +172,25 @@ public class Week04StringBuilderListSetMapLab {
 	
 	
 	// Method 3:
-	
-	
+	public static String shortestStringInList(List<String> listname) {
+		// Method 1: sorting the list by length, then returning the first entry
+		// Found from here: https://www.benchresources.net/java-8-how-to-sort-string-list-by-its-length/
+//		Collections.sort(listname, Comparator.comparingInt(String::length));
+//		return listname.getFirst();
+		
+		// Method 2: just iterate over the whole list and return the smallest length string
+		int smallestLength = listname.getFirst().length();
+		int smallestIndex = 0;
+		
+		for (int i = 1; i < listname.size(); i++) {
+			if (listname.get(i).length() < smallestLength) {
+				smallestIndex = i;
+				smallestLength = listname.get(i).length();
+			}
+		}
+		
+		return listname.get(smallestIndex);
+		
+	}
 
 }
